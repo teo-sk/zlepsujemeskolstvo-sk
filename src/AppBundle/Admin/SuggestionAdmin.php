371db939a8bd68d01f3b3719280186773bd127ce
Teo <teo@teo.sk>
2016-01-31 22:04:58 +0100

    added suggestion admin

---
+++ src/AppBundle/Admin/SuggestionAdmin.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class SuggestionAdmin extends Admin
+{
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('category')
+            ->add('approved')
+        ;
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('category')
+            ->add('email')
+            ->add('text')
+            ->add('created')
+            ->add('approved', null, array('editable' => true))
+            ->add('ip')
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'show' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                )
+            ))
+        ;
+    }
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('category')
+            ->add('email')
+            ->add('text')
+            ->add('created')
+            ->add('approved')
+            ->add('ip')
+            ->add('fingerprint')
+        ;
+    }
+
+    /**
+     * @param ShowMapper $showMapper
+     */
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('category')
+            ->add('email')
+            ->add('text')
+            ->add('created')
+            ->add('approved')
+            ->add('ip')
+            ->add('fingerprint')
+        ;
+    }
+}
