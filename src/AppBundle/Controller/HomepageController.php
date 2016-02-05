ddb2ab76b0d97ce72147baf0ac5066599d1aa9d3
Teo <teo@teo.sk>
2016-02-05 09:23:45 +0100

    Revert "fixed a bug where admin wouldnt get permission to newly created objects" Created issue #15 to handle this
    This reverts commit 53f5ad7b85ea973d925469d6fbd507c6e706004b.

--- src/AppBundle/Controller/HomepageController.php
+++ src/AppBundle/Controller/HomepageController.php
@@ -26,12 +26,8 @@ class HomepageController extends Controller
         if ($form->isSubmitted() && $form->isValid()) {
             $suggestion->setIp($request->getClientIp());
 
-            $admin = $this->get('app.admin.suggestion');
-
             $em->persist($suggestion);
             $em->flush();
-
-            $admin->createObjectSecurity($suggestion);
 
             $this->addFlash(
                         'form',
